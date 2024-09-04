dd2a52181ec573e55e6c51afc4c69f69992c5afb
emahji2683 <qurrrr3155@gmail.com>
2024-09-04 12:50:33 +0900

    ルーティングをルート

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  resources :content_lists, only: [:index, :show]
-  post 'result', to: 'repeat_string#result'
+  resources :content_lists, only: [:new, :create, :edit, :update, :destroy]
+  root to: 'content_lists#index'
 
   # Defines the root path route ("/")
   # root "posts#index"
