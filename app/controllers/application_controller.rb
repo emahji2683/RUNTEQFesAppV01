6a8f5fc33d69ddedbb1441cdd06693eeb15d11f8
emahji2683 <qurrrr3155@gmail.com>
2024-09-04 13:16:56 +0900

    nilでのエラーの抑止(appicationcont, times, str)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   def repeat_string(str, times)
+    if str.present? && times.present?
     # 指定された回数だけ文字列を繰り返し結合して返す
-    str * times
+      str * times
+    end
   end
 end
--- app/models/content_list.rb
+++ app/models/content_list.rb
@@ -1,2 +1,3 @@
 class ContentList < ApplicationRecord
+  
 end
