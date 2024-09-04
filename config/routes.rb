c93deb405820734bdb57d247e5e9ddb918ac9bc3
emahji2683 <qurrrr3155@gmail.com>
2024-09-04 12:29:23 +0900

    試験用ページ微修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-<<<<<<< HEAD
-=======
-  resources :content_lists, only: [:index, :show]
+  resources :content_lists
   post 'result', to: 'repeat_string#result'
->>>>>>> 1b5a560 (ルーティングの実施、テキストダウンロード機能の実装)
 
   # Defines the root path route ("/")
   # root "posts#index"
