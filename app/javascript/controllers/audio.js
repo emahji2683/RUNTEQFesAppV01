2f610f7acd733b591ee0a9acd4dbf09f7333f3c5
emahji2683 <qurrrr3155@gmail.com>
2024-09-04 18:15:48 +0900

    リンク生成機能の実装、音声出力機能の実装

--- app/javascript/controllers/audio.js
+++ app/javascript/controllers/audio.js
@@ -4,7 +4,7 @@
 export function speakText(text) {
   if (!text) return; // テキストが空の場合は何もしない
   const utterance = new SpeechSynthesisUtterance(text); // SpeechSynthesisUtteranceオブジェクトを作成
-  utterance.lang = 'ja-JP'; // 言語を日本語に設定
+  utterance.lang = 'bn-IN'; // 言語を日本語に設定
   window.speechSynthesis.speak(utterance); // 音声を再生
 }
 
