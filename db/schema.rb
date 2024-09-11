8846154fe6e157caf88ba04a15366ab30ced0e66
root <root@pR>
2024-09-11 16:37:26 +0900

    トップページとリンクの作成

--- db/schema.rb
+++ db/schema.rb
@@ -8,7 +8,7 @@
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
-# It's strongly recommended that you check this file into your version control system.
+#  It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.2].define(version: 2024_09_10_070427) do
   # These are extensions that must be enabled in order to support this database
