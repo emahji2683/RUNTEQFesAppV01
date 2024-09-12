776171b0581957091dee03d34bec27c669decf41
emahji2683 <qurrrr3155@gmail.com>
2024-09-12 12:22:59 +0900

    ゲストログインの複数担保、show画面のレイアウト修正

--- app/controllers/guest_sessions_controller.rb
+++ app/controllers/guest_sessions_controller.rb
@@ -1,11 +1,13 @@
 class GuestSessionsController < ApplicationController
-  def create
-    user = User.find_or_create_by(email: "guest@example.com") do |user|
-      user.password = SecureRandom.urlsafe_base64
-      user.name = "ゲストユーザー"
+    def create
+      user = User.create(
+        email: "guest_#{SecureRandom.hex(10)}@example.com",
+        password: SecureRandom.urlsafe_base64,
+        name: "ゲストユーザー"
+      )
+      session[:user_id] = user.id
+      flash[:notice] = "ゲストユーザーとしてログインしました"
+      redirect_to root_path
     end
-    session[:user_id] = user.id
-    flash[:notice] = "ゲストユーザーとしてログインしました"
-    redirect_to root_path
   end
-end
+  
